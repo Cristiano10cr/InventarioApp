507826c988b23e0da779f6cb00abef9b3316eb15
Santiago Alfonso <santiago.alfonso@aurotech.co>
2026-04-20 17:18:08 -0500

    Primer push con estructura base de proyecto y configuracion de git ignore

---
+++ ImventarioApp/Program.cs
@@ -0,0 +1,26 @@
+﻿// ============================================================
+// SISTEMA DE INVENTARIO - Clase 1.1
+// Estado: Mensaje de bienvenida
+// ============================================================
+using System.Reflection;
+ 
+    var assembly =Assembly.GetExecutingAssembly();
+    var version = assembly.GetName().Version;
+
+
+
+Console.WriteLine("==========================================");
+Console.WriteLine("    SISTEMA DE GESTIÓN DE INVENTARIO      ");
+Console.WriteLine("==========================================");
+Console.WriteLine();
+Console.WriteLine($"{version}");
+Console.WriteLine($"Versión: 1.0.0");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".NET Version: {Environment.Version}");
+Console.WriteLine();
+Console.WriteLine("Estructura de proyecto: ");
+Console.WriteLine("Configuracion .csproj: ");
+Console.WriteLine("Carpeta src/ creada");
+Console.WriteLine("Metadatos Configurados");
+Console.WriteLine();
+Console.WriteLine("Proximo paso: Agregar argumentos CLI' y confguracion de repositorio en github");
